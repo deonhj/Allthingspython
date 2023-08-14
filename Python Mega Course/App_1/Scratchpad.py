72cdab863e0a1f420ddabb425313b719f58835c6
Deon Jordaan <deonhjordaan@gmail.com>
2023-08-14 22:08:29 +0200

    add further day 16 changes

--- Python Mega Course/App_1/Scratchpad.py
+++ Python Mega Course/App_1/Scratchpad.py
@@ -1,27 +1,18 @@
-def parse(user_input):
-    """Extract the values split by a comma in a string
-    and return the two values via a dictionary.
-    """
-    # Get the two values in a list
-    parts = user_input.split(" ")
+import PySimpleGUI as sg
 
-    # Store the two values in variables
-    lower_bound = int(parts[0])
-    upper_bound = int(parts[1])
+label = sg.Text("What are dolphins?")
+option1 = sg.Radio("Amphibians", group_id="question1")
+option2 = sg.Radio("Fish", group_id="question1")
+option3 = sg.Radio("Mammals", group_id="question1")
+option4 = sg.Radio("Birds", group_id="question1")
 
-    # Inject the values in a dictionary
-    return {"lower_bound": lower_bound, "upper_bound": upper_bound}
+window = sg.Window("File Compressor",
+                   layout=[[label],
+                           [option1],
+                           [option2],
+                           [option3],
+                           [option4]
+                           ])
 
-
-import random
-
-# Ask the user to enter a lower and an upper bound divided by a comma
-user_input = input("Enter a lower bound and an uppwer bound divided a comma (e.g., 2,10)")
-
-# Parse the user string by calling the parse function
-parsed = parse(user_input)
-
-# Pick a random int between the two numbers
-rand = random.randint(parsed['lower_bound'], parsed['upper_bound'])
-
-print(rand)
+window.read()
+window.close()
