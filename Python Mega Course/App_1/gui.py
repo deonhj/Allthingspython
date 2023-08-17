9ed5f54aa77cb77636d1fee618966c5958d47c71
Deon Jordaan <deonhjordaan@gmail.com>
2023-08-17 10:25:22 +0200

    add various features to the gui

--- Python Mega Course/App_1/gui.py
+++ Python Mega Course/App_1/gui.py
@@ -4,9 +4,13 @@ import PySimpleGUI as Sg
 label = Sg.Text("Type in a todo")
 input_box = Sg.InputText(tooltip="Enter todo", key="todo")
 add_button = Sg.Button("Add")
+list_box = Sg.Listbox(values=functions.get_todos(),
+                      key='todos',
+                      enable_events=True, size=(45, 10))
+edit_button = Sg.Button("Edit")
 
 window = Sg.Window("My todo app",
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 16))
 while True:
     event, values = window.read()
@@ -17,6 +21,16 @@ while True:
             todos = functions.get_todos()
             todos.append(values['todo'] + "\n")
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = values['todo'] + "\n"
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
         case Sg.WIN_CLOSED:
             break
 window.close()
