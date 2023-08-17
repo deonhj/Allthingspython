d4ecadba38d437c763513a17b1da57a4215e8f8e
Deon Jordaan <deonhjordaan@gmail.com>
2023-08-17 10:01:53 +0200

    add add button

--- Python Mega Course/App_1/gui.py
+++ Python Mega Course/App_1/gui.py
@@ -1,10 +1,22 @@
 import functions
-import PySimpleGUI as sg
+import PySimpleGUI as Sg
 
-label = sg.Text("Type in a todo")
-input_box = sg.InputText(tooltip="Enter todo")
-add_button = sg.Button("Add")
+label = Sg.Text("Type in a todo")
+input_box = Sg.InputText(tooltip="Enter todo", key="todo")
+add_button = Sg.Button("Add")
 
-window = sg.Window("My todo app", layout=[[label], [input_box, add_button]])
-window.read()
+window = Sg.Window("My todo app",
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 16))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            todos.append(values['todo'] + "\n")
+            functions.write_todos(todos)
+        case Sg.WIN_CLOSED:
+            break
 window.close()
